030ea37b2cbc9d3de3af68d24cd8efa9f8738d9e
Otávio <buxiri@hotmail.com>
2018-08-26 16:38:30 -0300

    NewUser UTF-8

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+ï»¿import React, {Component} from 'react';
 
-import Label from '../Label/Label';
+import Label from '../Label';
 
 class NewUser extends Component {
   render() {
     return (
       <div className="center">
         <form className="pure-form pure-form-stacked">
-          <Label htmlFor="name" text="Quem é você?" />
+          <Label htmlFor="name" text="Quem Ã© vocÃª?" />
         </form>
       </div>
     );
